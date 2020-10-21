49a15e6cc3c40aa992bd0c30623cd0d6aad1a8fc
lustered <smellslikelavenders@gmail.com>
2020-10-21 05:02:44 -0400

    Added username. Missing Method chaining.

--- TestNim.java
+++ TestNim.java
@@ -1,4 +1,3 @@
-import java.util.HashMap ;
 import javax.swing.JOptionPane ;
 
 public class TestNim
@@ -14,13 +13,10 @@ public class TestNim
         // Options for JOptionPane.
         Object[] menu = { "No", "Yes" } ;
         Object[] availablePlayers = { "Human", "Smart Computer", "Dumb Computer" } ;
+        
+        // Constructors for the concrete classes.
+        Player[] builders = { new Human(), new SmartComputer(), new AverageComputer() };
 
-        // TODO : This can be refactored so much better without using hashmap.
-        // Pair of Player and constructors.
-        HashMap<String, Player> options = new HashMap<>() ;
-        options.put("Human", new Human()) ;
-        options.put("Smart Computer", new SmartComputer()) ;
-        options.put("Dumb Computer", new AverageComputer()) ;
 
         // Array of the interface Player.
         Player[] players = new Player[size] ;
@@ -29,20 +25,21 @@ public class TestNim
 
         do
         {
-            // Ask the user the players he wants to play.
+            // For n player that wants to play.
             for (int i = 0 ; i < size ; i++) 
             {
+                // Ask the user the players he wants to play.
                 userOption = JOptionPane.showOptionDialog(null, "Select player #: " + (i+1), 
                         "Nim Game", JOptionPane.DEFAULT_OPTION,
                         JOptionPane.WARNING_MESSAGE, null, availablePlayers, availablePlayers[0]) ;
 
                 // Get the name of the player.
-                String name = (JOptionPane.showInputDialog("Insert the name for player #: " + (i+1)));
+                String userName = (JOptionPane.showInputDialog("Insert the name for player #: " + (i+1)));
                 
                 // Add the addecuate constructor for the chosen player.
-                players[i] = options.get(availablePlayers[userOption]);
+                players[i] = builders[userOption];
                 // Setting the name of the player.
-                players[i].setPlayerName(name);
+                players[i].setPlayerName(userName);
 
             }
 
@@ -53,7 +50,7 @@ public class TestNim
             System.out.println(game.play()) ;
 
             // Ask the user if they want to play again.
-            playAgain = JOptionPane.showOptionDialog(null, "Play again?", 
+            playAgain = JOptionPane.showOptionDialog(null, "Do you want to play again?", 
                     "Nim Game", JOptionPane.DEFAULT_OPTION,
                     JOptionPane.WARNING_MESSAGE, null, menu, menu[0]) ;
 
