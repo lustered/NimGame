b219c629cde8f8c783017e485e1060167ba0224e
lustered <smellslikelavenders@gmail.com>
2020-10-22 13:12:23 -0400

    .

--- BelowAverageComputer.java
+++ BelowAverageComputer.java
@@ -2,29 +2,53 @@ import java.util.Random ;
 
 class BelowAverageComputer implements Player
 {
+    // Var to set the user/player name.
     private String playerName ;
     private final Random rnd = new Random() ;
 
+    /** 
+     * Constructor for {@link BelowAverageComputer}.
+     */
     public BelowAverageComputer()
     {
         playerName = "AverageComputer";
     }
 
+    /** 
+     * Constructor for {@link BelowAverageComputer}.
+     */
     public BelowAverageComputer(String playerName)
     {
         this.playerName = playerName ;
     }
 
+    /**
+     * Method to handle user input and check proper range.
+     *
+     * @param marbles amount of marbles left in the pile class.
+     * @return amount of marbles to remove.
+     */
     public int move(int marbles)
     {
        return rnd.nextInt(marbles / 2) + 1 ;
     }
 
+
+    /**
+     * Set the player's name.
+     *
+     * @param playerName {@link String} set the player's name.
+     */
     public void setPlayerName(String playerName)
     {
         this.playerName = playerName;
     }
 
+    /**
+     * Get the player's name.
+     * 
+     * @return Player's name.
+     */
     public String getPlayerName()
     {
         return playerName ;
--- Pile.java
+++ Pile.java
@@ -2,25 +2,46 @@ import java.util.Random ;
 
 public class Pile
 {
+    // Random to get random pile size.
     private final Random rnd = new Random() ;
+    // Marbles in the pile.
     private int marbles ; 
+    // Last amount of marbles removed.
     private int lastPlay ;
 
+    /**
+     * Constructor for {@link Pile}.
+     */
     public Pile()
     {
         marbles = rnd.nextInt(91)+10;
     }
 
+    /**
+     * Get current number of marbles in the pile.
+     * 
+     * @return marbles in the pile.
+     * */
     public int getMarbles()
     {
         return marbles ;
     }
 
+    /**
+     * Get last amount removed.
+     *
+     * @return last amount of marbles removed from the pile.
+     */
     public int getLastPlay()
     {
         return lastPlay ;
     }
 
+    /**
+     * Remove specified amount of marbles from the pile.
+     *
+     * @param amount specified amount to remove. 
+     */
     public void removeMarbles(int amount) throws ArithmeticException
     {
         // Check if amount to substract from the pile is legal.
@@ -34,6 +55,10 @@ public class Pile
             throw new ArithmeticException("To dev. The amount of marbles returned by the 'move' method is not legal: " + amount );
     }
 
+    /**
+     * Check if there are marbles in the pile.
+     * @return true if there are marbles, else false.
+     */
     public boolean hasMarbles()
     {
         return marbles > 1 ;
